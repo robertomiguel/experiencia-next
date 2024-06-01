ba0f9a577d05daf61bb1afd8b0779ea73dc20c6c
Roberto <roberto@MacBook-Air-de-Roberto.local>
2024-06-01 18:10:09 -0300

    fix headers 2

--- src/app/api/crypto/route.ts
+++ src/app/api/crypto/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
     const url = `${process.env.TV_INFO}/?symbol=${symbol}&locale=en`;
 
     const headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept': '*/*',
         'Accept-Language': 'en-US,en;q=0.9,es;q=0.8',
         'Cache-Control': 'max-age=0',
         'Sec-CH-UA': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
@@ -19,7 +19,9 @@ export async function GET(request: NextRequest) {
         'Sec-Fetch-Site': 'none',
         'Sec-Fetch-User': '?1',
         'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+        'Origin': 'https://www.tradingview.com',
+        'Referer': 'https://www.tradingview.com',
     }
 
     const response = await axios.get(url, { headers });
