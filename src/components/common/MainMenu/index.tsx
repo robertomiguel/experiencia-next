bb860af28c2b79a51abed5122542f9c24fea6236
Roberto Miguel <robertomiguel@MacBook-Pro-de-Roberto.local>
2025-02-27 17:57:58 -0300

    text fabric

--- src/components/common/MainMenu/index.tsx
+++ src/components/common/MainMenu/index.tsx
@@ -7,10 +7,11 @@ export const MainMenu = () => {
     { label: "Art", href: "/art" },
     { label: "Image", href: "/image" },
     { label: "Editor", href: "/editor" },
-    { label: 'Paint', href: '/paint' },
+    { label: "Paint", href: "/paint" },
+    { label: "Text", href: "/text" },
   ];
   return (
-    <nav className="flex z-20 text-gray-50 bg-blue-800 p-2 pl-4 pr-4 m-3 rounded-full gap-3 sticky top-0">
+    <nav className="flex z-20 text-gray-50 bg-blue-800 p-2 pl-4 pr-4 m-3 rounded-full gap-3 sticky top-0 overflow-x-auto ">
       {menuItems.map((item) => (
         <LinkAction key={item.href} label={item.label} href={item.href} />
       ))}
